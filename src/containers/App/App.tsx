b2d0c3f61b07c846f3dedf9b897e5597fa4ed52e
Nurzat Taalaibekova <tnurzat91@gmail.com>
2023-10-30 08:14:37 +0300

    Changing styles and game display

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,32 +1,60 @@
-import React, {useState} from 'react';
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
 interface Cell {
   hasItem: boolean;
   clicked: boolean;
 }
-function App() {
-  const createItems = () =>{
-    const cells: Cell[] = [];
 
-    for (let i = 0; i < 36; i++) {
-      cells.push({ hasItem: false, clicked: false });
-    }
-    return cells;
+function createItems() {
+  const cells: Cell[] = [];
+
+  for (let i = 0; i < 36; i++) {
+    cells.push({ hasItem: false, clicked: false });
+  }
+
+  const randomIndex = Math.floor(Math.random() * cells.length);
+  cells[randomIndex].hasItem = true;
+
+  return cells;
+}
+
+function App() {
+  const [items, setItems] = useState(createItems());
+  const [attempts, setAttempts] = useState(0);
+
+  const resetGame = () => {
+    setItems(createItems());
+    setAttempts(0);
   };
-  console.log(createItems());
-  const cellsArray = createCellsArray();
-  // const [items, setItems] = useState(createItems());
+
+  const onItemClick = (index: number) => {
+    const updatedItems = [...items];
+    updatedItems[index] = { ...updatedItems[index], clicked: true };
+    setItems(updatedItems);
+    setAttempts(attempts + 1);
+  };
 
   return (
     <div>
-      {cellsArray.map((cell, index) => (
-        <div key={index}>
-          {`Cell ${index + 1}: hasItem - ${cell.hasItem.toString()}, clicked - ${cell.clicked.toString()}`}
-        </div>
-      ))}
+      <div className="game-container">
+        {items.map((cell, index) => (
+          <div
+            key={index}
+            className={`cell ${cell.clicked ? (cell.hasItem ? 'has-item' : '') : ''}`}
+            onClick={() => onItemClick(index)}
+          >
+            {cell.clicked ? (cell.hasItem ? 'O' : '') : ''}
+          </div>
+        ))}
+      </div>
+      <div className="info">
+        <p>Количество попыток: {attempts}</p>
+        <button onClick={resetGame}>Сброс</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
